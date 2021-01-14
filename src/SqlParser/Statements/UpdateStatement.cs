8fd9e8d87a03d8195652c23ff2fd240ac5dacddf
hishamco <hishamco_2007@yahoo.com>
2021-01-15 00:00:21 +0300

    Add update statement

---
+++ src/SqlParser/Statements/UpdateStatement.cs
@@ -0,0 +1,110 @@
+﻿using Parlot;
+using Parlot.Fluent;
+using SqlParser.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Parlot.Fluent.Parsers;
+
+namespace SqlParser.Statements
+{
+    /*
+     * updateStatement ::= UPDATE tableName SET columnsAssignment
+     * 
+     * tableName ::= identifier
+     * 
+     * columnsAssignment ::= columnAssignment (, columnAssignment)*
+     * 
+     * columnAssignment ::= columnName = expression
+     */
+    public class UpdateStatement : Statement
+    {
+        protected static readonly Parser<char> Equal = Terms.Char('=');
+        protected static readonly Parser<char> Comma = Terms.Char(',');
+
+        protected static readonly Parser<string> True = Terms.Text("True", caseInsensitive: true);
+        protected static readonly Parser<string> False = Terms.Text("False", caseInsensitive: true);
+
+        protected static readonly Parser<string> Update = Terms.Text("UPDATE", caseInsensitive: true);
+        protected static readonly Parser<string> Set = Terms.Text("SET", caseInsensitive: true);
+
+        private static readonly Deferred<IEnumerable<Token>> _tokens = Deferred<IEnumerable<Token>>();
+
+        public string TableName { get; private set; }
+
+        public IEnumerable<string> ColumnNames { get; set; }
+
+        public IEnumerable<object> Values { get; set; }
+
+        static UpdateStatement()
+        {
+            var number = Terms.Decimal(NumberOptions.AllowSign)
+                .Then<Expression>(e => new NumericExpression(e));
+            var boolean = True.Or(False)
+                .Then<Expression>(e => new BooleanExpression(Convert.ToBoolean(e)));
+            var stringLiteral = Terms.String(StringLiteralQuotes.SingleOrDouble)
+                .Then<Expression>(e => new LiteralExpression(e.ToString()));
+            var identifier = Terms.Identifier()
+                .Then<Expression>(e => new IdentifierExpression(e.ToString()));
+            var terminal = number.Or(boolean).Or(stringLiteral).Or(identifier);
+            var columnsAssignment = Separated(Comma, identifier.And(Equal).And(terminal));
+            var upateStatement = Update.And(identifier).And(Set).And(columnsAssignment);
+            _tokens.Parser = upateStatement.Then<IEnumerable<Token>>(e =>
+            {
+                var tableName = (e.Item2 as IdentifierExpression).Name;
+                var columns = e.Item4.Select(a => (a.Item1 as IdentifierExpression).Name);
+                var values = e.Item4.Select(a => {
+                    object value = null;
+                    if (a.Item3 is IdentifierExpression)
+                    {
+                        value = (a.Item3 as IdentifierExpression).Name;
+                    }
+                    else if (a.Item3 is BooleanExpression)
+                    {
+                        value = (a.Item3 as BooleanExpression).EvaluateAsync().GetAwaiter().GetResult().ToBooleanValue();
+                    }
+                    else if (a.Item3 is NumericExpression)
+                    {
+                        value = (a.Item3 as NumericExpression).EvaluateAsync().GetAwaiter().GetResult().ToNumberValue();
+                    }
+                    else if (a.Item3 is LiteralExpression)
+                    {
+                        value = (a.Item3 as LiteralExpression).EvaluateAsync().GetAwaiter().GetResult().ToStringValue();
+                    }
+
+                    return value;
+                }).ToArray();
+
+                return new List<Token>
+                {
+                    new Token { Type = TokenType.Keyword, Value = e.Item1 },
+                    new Token { Type = TokenType.Identifier, Value = tableName },
+                    new Token { Type = TokenType.Keyword, Value = e.Item3 },
+                    new Token { Type = TokenType.List, Value = columns },
+                    new Token { Type = TokenType.List, Value = values }
+                };
+            });
+        }
+
+        public UpdateStatement(string commandText) : base(commandText)
+        {
+
+        }
+
+        public async override Task TokenizeAsync()
+        {
+            var context = new SqlParseContext(CommandText);
+            var result = new ParseResult<IEnumerable<Token>>();
+
+            _tokens.Parse(context, ref result);
+
+            Tokens = result.Value;
+            TableName = Tokens.ElementAt(1).Value.ToString();
+            ColumnNames = (IEnumerable<string>)Tokens.ElementAt(3).Value;
+            Values = (IEnumerable<object>)Tokens.ElementAt(4).Value;
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ test/SqlParser.Tests/UpdateStatementTests.cs
@@ -0,0 +1,30 @@
+﻿using SqlParser.Statements;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SqlParser.Tests
+{
+    public class UpdateStatementTests
+    {
+        [Theory]
+        [InlineData("Update People Set Age = 32", "People", new[] { "Age" }, new object[] { 32 })]
+        [InlineData("Update People Set FirstName = 'Jon', LastName = 'Doe'", "People", new [] { "FirstName", "LastName" }, new object[] { "Jon", "Doe" })]
+        public async Task ParseUpdateStatement(string text, string expectedTableName, string[] expectedColumnNames, object[] expectedValues)
+        {
+            // Arrange
+            var statement = new UpdateStatement(text);
+
+            // Act 
+            await statement.TokenizeAsync();
+
+            // Assert
+            Assert.Equal(5, statement.Tokens.Count());
+            Assert.Equal("UPDATE", statement.Tokens.ElementAt(0).Value);
+            Assert.Equal("SET", statement.Tokens.ElementAt(2).Value);
+            Assert.Equal(expectedTableName, statement.TableName);
+            Assert.Equal(expectedColumnNames, statement.ColumnNames);
+            Assert.True(expectedValues.SequenceEqual(statement.Values, new SequenceComparer()));
+        }
+    }
+}
